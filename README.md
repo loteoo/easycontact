cf7012e3e9dad20cb1dd3ad394bf0e7f36e55816
Alexandre Lotte <alexlotte16@gmail.com>
2017-09-26 16:27:46 -0400

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# easycontact
-Quick contact class to complement basic contact forms. This is back-end skeleton code that I got tired of rewriting for new small projects. Kept it all simple for easy maintenance.
+# Easy Contact
+
+Quick contact class to complement basic contact forms.
+This is back-end skeleton code that I got tired of rewriting for new small projects.
+Kept it all simple for easy maintenance.
+
+Works well with ajax forms
--- contact.php
+++ contact.php
@@ -11,8 +11,8 @@
 
     public $sendMail = true; // Toggles mail sending function
     public $saveJson = true; // Toggles json saving function
-    public $mailTo; // Person who will reveive the messages
     public $submission = array(); // Asso. array, generally contains form POST data
+    public $mailTo; // Person who will reveive the messages
 
     function __construct($submission, $mailTo) {
       if (is_array($submission)) {
@@ -26,18 +26,19 @@
     // Launch toggled functions
     public function go() {
 
-      // Add data to the submission
+      // Add extra data to the submission
       setlocale(LC_TIME, 'fr_CA.UTF-8');
       $this->submission["ip"] = $_SERVER['REMOTE_ADDR'];
       $this->submission["date"] = strftime("%B %e %G, %I:%M %p");
 
-
+      // Send mail if toggled
       if ($this->sendMail) {
         if (!$this->sendMail($this->submission, $this->mailTo)) {
           return false;
         }
       }
 
+      // Save JSON if toggled
       if ($this->saveJson) {
         if (!$this->saveJson($this->submission)) {
           return false;
@@ -48,21 +49,30 @@
     }
 
 
-
+    // Sends an email to a specified person, containing submitted data.
     public function sendMail($submission, $mailTo) {
+
+      // Destination email required
       if (!$mailTo) {
         return false;
       }
+
+      // Something to send required
       if (!is_array($submission)) {
         return false;
       }
+
+
+      // Alter / cleanup form data here
       $submittedEmail = strip_tags($submission["email"]);
       $subject = strip_tags($submission["subject"]);
+      $message = strip_tags($submission["message"]);
+
 
       // Here you can customise the email body.
-      $msg = "Message from " . $submittedEmail . "\n\n";
-      $msg .= wordwrap(strip_tags($submission["message"]), 70);
-      $msg .= "\n\nSent from " . $_SERVER['HTTP_HOST'];
+      $emailContent = "Message from " . $submittedEmail . "\n\n";
+      $emailContent .= wordwrap($message, 70);
+      $emailContent .= "\n\nSent from " . $_SERVER['HTTP_HOST'];
 
 
       $encoding = "utf-8";
@@ -73,6 +83,7 @@
            "line-length" => 76,
            "line-break-chars" => "\r\n"
        );
+
        // Mail header
        $header = "Content-type: text/html; charset=".$encoding." \r\n";
        $header .= "From: $submittedEmail <$submittedEmail> \r\n";
@@ -85,10 +96,11 @@
       // Send the email using basic mail() function.
       // You should whitelist this domain in your mailbox so messages dont go in SPAM folder,
       // or use another (proper) method of mailing.
-      return mail($mailTo, $subject, $msg, $header);
+      return mail($mailTo, $subject, $emailContent, $header);
     }
 
 
+    // Saves submitted data to a json file.
     public function saveJson($submission) {
       // Some data is obviously required if you want to save it
       if (!is_array($submission)) {
@@ -123,19 +135,28 @@
   }
 
 
+
+
+// =========================
+// Basic usage example here
+// =========================
+
 // Required fields
 if (array_key_exists('email', $_POST) && array_key_exists('subject', $_POST) && array_key_exists('message', $_POST)) {
 
-
+    // Instantiate
     $quickContact = new EasyContact($_POST, "alexlotte16@gmail.com");
+
+    // Chose settings, defaults: sendMail = true, saveJson = true
     $quickContact->sendMail = false;
 
+    // Launch toggled functions and check for failure
+    // better error management when i'll care.. probably never
     if ($quickContact->go()) {
       echo "Success";
     } else {
       echo "Error";
     }
-
 
 }
 
